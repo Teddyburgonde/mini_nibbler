262c233cc8b448b88e7b0840f0461a4db8b7c566
Teddyburgonde <bandama.teddy@gmail.com>
2025-07-24 09:18:41 +0200

    added IGui.hpp, Input.hpp

---
+++ includes/IGui.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "GameState.hpp"
+#include "Input.hpp"
+
+/**
+ * @class IGui
+ * @brief Interface abstraite pour toutes les interfaces graphiques du projet (ncurses, SFML, SDL…).
+ *
+ * Cette interface définit un contrat minimal que chaque GUI doit respecter :
+ * - Initialiser une fenêtre avec une taille donnée.
+ * - Afficher l’état courant du jeu.
+ * - Récupérer les entrées utilisateur (clavier).
+ * - Libérer proprement les ressources utilisées.
+ *
+ * Toute classe implémentant cette interface pourra être interchangeable dans le moteur de jeu.
+ */
+class IGui 
+{
+	public:
+		virtual void init(int width, int height) = 0;
+		virtual void render(const GameState& state) = 0;
+		virtual Input getInput() = 0;
+		virtual void cleanup() = 0;
+		virtual ~IGui(){};
+};
---
+++ includes/Input.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+/**
+ * @file Input.hpp
+ * @brief Enumération des entrées clavier utilisées dans le jeu.
+ *
+ * Cette énumération représente les différentes directions possibles
+ * pour le serpent ainsi que les actions spéciales comme quitter le jeu.
+ *
+ * Elle est utilisée pour unifier la gestion des entrées utilisateur
+ * dans toutes les interfaces graphiques (ncurses, SDL, SFML...).
+ */
+enum Input {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	EXIT,
+	NONE
+};
