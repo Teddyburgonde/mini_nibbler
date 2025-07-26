5f24a2a6936d0a78f0a21c03fe1a45c2a52b985c
Teddyburgonde <bandama.teddy@gmail.com>
2025-07-26 23:57:29 +0200

    added commentary in entrypoint.cpp

--- gui_ncurses/entrypoint.cpp
+++ gui_ncurses/entrypoint.cpp
@@ -1,5 +1,13 @@
 #include "GuiNcurses.hpp"
 
+/**
+ * @brief Point d'entrée utilisé par dlsym() pour créer dynamiquement l'objet GUI.
+ *
+ * Cette fonction est exportée sans nom mangling grâce à `extern "C"`,
+ * ce qui permet à `dlsym()` de la retrouver dans la bibliothèque partagée (.so).
+ *
+ * @return Un pointeur vers un objet GuiNcurses qui implémente l'interface IGui.
+ */
 extern "C" IGui* createGui() 
 { 
 	return new GuiNcurses();
